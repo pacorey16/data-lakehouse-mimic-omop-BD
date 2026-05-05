93a0aa172f99a60b9e50d11f19f33092c45236cc
palobrav <palbradel@alum.us.es>
2026-05-05 14:02:31 +0200

    Corrección de errores

--- docker/trino/init_mimic_bronze.sql
+++ docker/trino/init_mimic_bronze.sql
@@ -1,7 +1,43 @@
 -- 1. Esquemas
-CREATE SCHEMA IF NOT EXISTS iceberg.omop_silver WITH (location = 's3://omop-silver/');
-CREATE SCHEMA IF NOT EXISTS hive.mimic_bronze
-WITH (location = 's3://mimic-bronze/');
+CREATE SCHEMA IF NOT EXISTS hive.mimic_bronze WITH (location = 's3://mimic-bronze/');
+
+-- Schema omop en iceberg apuntando a S3 (drop si existe con ruta local incorrecta)
+DROP SCHEMA IF EXISTS iceberg.omop CASCADE;
+CREATE SCHEMA IF NOT EXISTS iceberg.omop WITH (location = 's3://omop-silver/');
+
+-- Schema vocabulario OMOP (tablas CSV externas)
+CREATE SCHEMA IF NOT EXISTS hive.vocabulary WITH (location = 's3://mimic-vocabulary/');
+
+-- 1b. Tablas de vocabulario OMOP
+CREATE TABLE IF NOT EXISTS hive.vocabulary.concept (
+    concept_id        VARCHAR,
+    concept_name      VARCHAR,
+    domain_id         VARCHAR,
+    vocabulary_id     VARCHAR,
+    concept_class_id  VARCHAR,
+    standard_concept  VARCHAR,
+    concept_code      VARCHAR,
+    valid_start_date  VARCHAR,
+    valid_end_date    VARCHAR,
+    invalid_reason    VARCHAR
+) WITH (
+    format = 'CSV',
+    external_location = 's3://mimic-vocabulary/concept/',
+    skip_header_line_count = 1
+);
+
+CREATE TABLE IF NOT EXISTS hive.vocabulary.concept_relationship (
+    concept_id_1     VARCHAR,
+    concept_id_2     VARCHAR,
+    relationship_id  VARCHAR,
+    valid_start_date VARCHAR,
+    valid_end_date   VARCHAR,
+    invalid_reason   VARCHAR
+) WITH (
+    format = 'CSV',
+    external_location = 's3://mimic-vocabulary/concept_relationship/',
+    skip_header_line_count = 1
+);
 
 -- 2. Tabla Admissions
 CREATE TABLE IF NOT EXISTS hive.mimic_bronze.admissions (
